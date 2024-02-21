769218eece3c1eb5e8db32f34403f159555cf33a
Jettison <jettisonjj@gmail.com>
2024-02-21 23:54:23 +0800

    Initialize DbContext

---
+++ SleekWH/Models/WhContext.cs
@@ -0,0 +1,16 @@
+
+using SleekWH.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SleekWH.Data
+{
+    public class SleekContext : DbContext
+    {
+        public SleekContext(DbContextOptions<SleekContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ItemModel> ItemModels { get; set; }
+    }
+}
