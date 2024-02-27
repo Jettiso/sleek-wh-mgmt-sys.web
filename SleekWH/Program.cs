e47bfa631ca28509e32e8ac61687a638622a167f
Jettison <jettisonjj@gmail.com>
2024-02-27 23:55:57 +0800

    removed Cors Policy for the meantime for testing

--- SleekWH/Program.cs
+++ SleekWH/Program.cs
@@ -10,18 +10,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-builder.Services.AddCors(options =>
-{
-	options.AddPolicy("Localhost",
-		policy =>
-		{
-			policy.WithOrigins("http://localhost:5173/").AllowAnyHeader().AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
-		});
-});
+
 
 
 var app = builder.Build();
-
 
 app.UseHttpsRedirection();
 app.MapControllers();
