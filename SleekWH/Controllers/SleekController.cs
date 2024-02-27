dc471003d833891fb291fe793abfdc5a603e454e
Jettison <jettisonjj@gmail.com>
2024-02-27 23:55:05 +0800

    tested controllers through postman

--- SleekWH/Controllers/SleekController.cs
+++ SleekWH/Controllers/SleekController.cs
@@ -1,7 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Cors;
 using SleekWH.Data;
 using SleekWH.Models;
 
@@ -18,23 +17,13 @@ namespace sleekWH.Controllers
         {
             _sleekcontext = sleekcontext;
         }
-        [EnableCors("Localhost")]
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ItemModel>>> GetItems()
         {
             var items = await _sleekcontext.ItemModels.ToListAsync();
 
             return Ok(items);
-        }
-        
-        [EnableCors("Localhost")]
-        [HttpPost]
-        public async Task<IActionResult> PostItem([FromBody] ItemModel item)
-        {
-            _sleekcontext.ItemModels.Add(item);
-            await _sleekcontext.SaveChangesAsync();
-
-            return Ok(item);
         }
     }
 }
